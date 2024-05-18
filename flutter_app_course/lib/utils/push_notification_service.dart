b2e4bcfe2abfc47c8ffbf5bc8acd9baaa25b2af2
gustavsr1994 <gustavr1994@gmail.com>
2024-05-18 10:14:26 +0700

    push notification with Firebase

--- flutter_app_course/lib/utils/push_notification_service.dart
+++ flutter_app_course/lib/utils/push_notification_service.dart
@@ -1,15 +1,34 @@
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 
 class PushNotificationService {
   final FlutterLocalNotificationsPlugin _localNotificationsPlugin =
       FlutterLocalNotificationsPlugin();
+  FirebaseMessaging _firebaseMessaging = FirebaseMessaging.instance;
 
   Future initialize() async {
+    var tokenFCM = await _firebaseMessaging.getToken();
+    print('Token FCM : $tokenFCM');
+    FirebaseMessaging.onBackgroundMessage(
+        (message) => _firebaseMessageBackground(message));
+    NotificationSettings notificationSettings =
+        await _firebaseMessaging.requestPermission();
+
     AndroidInitializationSettings androidInitializationSettings =
         AndroidInitializationSettings('@mipmap/ic_launcher');
     InitializationSettings initializationSettings =
         InitializationSettings(android: androidInitializationSettings);
     await _localNotificationsPlugin.initialize(initializationSettings);
+
+    FirebaseMessaging.onMessage.listen((message) {
+      showNotification(
+          message.notification!.title ?? '', message.notification!.body ?? '');
+    });
+  }
+
+  Future<void> _firebaseMessageBackground(RemoteMessage message) async {
+    showNotification(
+        message.notification!.title ?? '', message.notification!.body ?? '');
   }
 
   void showNotification(String title, String body) {
